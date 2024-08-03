b61983c8b079b8e3226561cb5d316fbcf2b7c6b4
moez-rd <slow9ie@gmail.com>
2024-08-03 21:06:26 +0700

    update: add completed functions in main

--- Publication/main.py
+++ Publication/main.py
@@ -2,6 +2,9 @@ import os
 
 from src.check_oai_links import run as check_oai_links
 from src.scrape_xml import run as scrape_xml
+from src.extract_journals import run as extract_journals
+from src.check_relation_links import run as check_relation_links
+from src.download_pdf import run as download_pdf
 
 def main():
     if not os.path.exists("data"): 
@@ -9,6 +12,9 @@ def main():
 
     check_oai_links()
     scrape_xml()
+    extract_journals()
+    check_relation_links()
+    download_pdf()
 
 if __name__ == "__main__":
     main()
