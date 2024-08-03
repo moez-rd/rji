47bd05d561a34a9bccd7a1e5ff8385f36ac9304a
moez-rd <slow9ie@gmail.com>
2024-08-03 19:12:09 +0700

    feat: check if data dir not exists

--- Publication/main.py
+++ Publication/main.py
@@ -1,7 +1,13 @@
+import os
+
 from src.check_oai_links import run as check_oai_links
 from src.scrape_xml import run as scrape_xml
 
 def main():
+    if not os.path.exists("data"): 
+        os.makedirs("data")
+
+    check_oai_links()
     scrape_xml()
 
 if __name__ == "__main__":
